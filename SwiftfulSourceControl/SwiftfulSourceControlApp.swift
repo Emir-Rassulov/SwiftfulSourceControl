015c6b20eeecb4bc585d344a834c856faaca0bf9
Emir-Rassulov <emir.rassulov@gmail.com>
2024-06-07 20:25:07 +0200

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Emir Rassulov on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
