aa792a390b41bc29cc7124da23376ccfad709cf0
Emir-Rassulov <emir.rassulov@gmail.com>
2024-06-07 21:32:04 +0200

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Emir Rassulov on 07/06/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
