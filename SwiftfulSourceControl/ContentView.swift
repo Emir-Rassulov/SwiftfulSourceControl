a6e86e38053a4c4ee7fa32a2cf2d46869fb2d1d7
Emir-Rassulov <emir.rassulov@gmail.com>
2024-06-08 15:52:29 +0200

    [Feature] Subscribe button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -43,12 +43,12 @@ struct ContentView: View {
             ScrollView {
                 VStack {
                     ForEach(0..<5) { _ in
-                        Image(systemName: "magnifyingglass")
+                        Image(systemName: "globe")
                             .font(.largeTitle)
                             .foregroundStyle(.tint)
                         Text("Swiftful Thinking!")
                         
-                        Button ("Click me!") {
+                        Button ("Subscribe now!") {
                             
                         }
                     }
